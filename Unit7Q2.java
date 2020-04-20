26e3b71ef1bbd0ac1f8981a930eca67ca97220ff
danan04 <54724801+danan04@users.noreply.github.com>
2020-04-20 02:45:27 -0700

    add unit7 q2

---
+++ Unit7Q2.java
@@ -0,0 +1,13 @@
+/*Consider the following statement, which is intended to create an ArrayList named numbers that can be used to store Integer values.
+
+ArrayList<Integer> numbers = missing code;
+
+Which of the following can be used to replace  missing codeso that the statement works as intended?
+
+I.new ArrayList()
+II.new ArrayList<Integer>
+III.new ArrayList<Integer>()*/
+I and III 
+I --> declares ArrayList object that has integer Objects
+II --> syntax error
+III --> declares ArrayList object that has elements of type Integer
